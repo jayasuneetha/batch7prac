30514bebe4a3598fa61bb2bd62b0ac9fb311130b
email <suee535@gmail.com>
2022-07-14 13:43:50 +0530

    Removed one SOP statement

--- sample.java
+++ sample.java
@@ -1,1 +1,21 @@
+//simple java program ti disply message
+import java.lang.*;
+class sample
+{
+public static void main(string args[])
+{
+        system.out.println("This is added in master branch");
+system.out.println ("welcome Batch 7 Devops Team");
+system.out.println ("unwanted line");
+system.out.println ("Thank you for joining devops course");
+system.out.println ("Added extra line");
+system.out.println ("Date & Time not used : ");
+system.out.println ("This is from sunitha branch");
+system.out.println ("Thank you for joining devops course");
+system.out.println ("Added extra line");
+//This is a comment at the end of main()
+//One more comment line
+system.out.println("Hello")
+}
+}
 
